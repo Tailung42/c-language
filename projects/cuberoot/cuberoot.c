4b36c01708dcec21983395e3c97535e86e22c60a
Bipin Thapa <142653093+Tailung42@users.noreply.github.com>
2024-05-26 20:50:16 +0545

    added cuberoot function

--- projects/cuberoot/cuberoot.c
+++ projects/cuberoot/cuberoot.c
@@ -5,11 +5,8 @@
 double qroot(double num)
 {
     double a, b , c;
-    for ( a = 0; f(a, num) * f(a + 1, num) > 0; a++)
-    {
-
-    }
-        b = a + 1;
+    for ( a = 0; f(a, num) * f(a + 1, num) > 0; a++){}
+     b = a + 1;
 
     c = (a + b)/ 2;
     while ( (f(c, num) > 0 && f(c, num) > epsilon) || (f(c, num) < 0 && f(c, num) < -epsilon))
--- projects/cuberoot/main.c
+++ projects/cuberoot/main.c
@@ -7,6 +7,6 @@ int main(void)
     printf("Enter a number to find cube root:  ");
     scanf("%f", &num);
     double result = qroot(num);
-    printf("CubeRoot: %lf", result);
+    printf("CubeRoot: %.3lf", result);
     return 0;
 }
