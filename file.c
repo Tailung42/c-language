3dd9e5175b06b5954e28bf1e222bd867b0701ac1
Bipin Thapa <142653093+Tailung42@users.noreply.github.com>
2024-06-01 21:56:48 +0545

    made changes

--- file.c
+++ file.c
@@ -5,6 +5,5 @@ int main(void)
   printf("hello world");
 
   int a = 1;
-  printf("this change was made in branch shit");
-  printf("changes from source");
+  printf("this change was made in branch style");
 }
